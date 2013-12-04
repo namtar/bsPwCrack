2078aff90f12d91e51aee73fe01aeb3cd8b8f264
namtar <matthias.drummer@gmail.com>
2013-12-04 23:02:50 +0100

    added basic methods

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: drummer
+ *
+ * Created on 4. Dezember 2013, 21:47
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
